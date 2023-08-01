38b7553f6f9464f4c4c0bf9710c8150cbc3306c8
Peter Duerst <peterad12@gmail.com>
2023-07-31 21:36:04 -0400

    Imported progress from scratch website

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,43 @@
+<h1>About Petal Quest</h1>
+<p>This page will contain info about who we are.</p>
+<p>
+	For now, this is just a page with a ton of content so we can test how things look with lots of
+	content.
+</p>
+<p class="filler">
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, praesentium eos et
+	consequuntur neque similique, est velit saepe tempora nobis architecto commodi cum, natus ea
+	reprehenderit voluptatum mollitia eligendi porro.
+</p>
+<p class="filler">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quam culpa beatae nesciunt aliquam,
+	porro ipsum accusamus id corporis laborum vitae quasi ut odit sequi labore? Recusandae vero ullam
+	tempore!
+</p>
+<p class="filler">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem quasi dolore inventore eum
+	voluptas provident molestiae! Inventore minima, laborum illo enim hic ratione maxime possimus.
+	Asperiores rem beatae illo?
+</p>
+<p class="filler">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem quasi dolore inventore eum
+	voluptas provident molestiae! Inventore minima, laborum illo enim hic ratione maxime possimus.
+	Asperiores rem beatae illo?
+</p>
+<p class="filler">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem quasi dolore inventore eum
+	voluptas provident molestiae! Inventore minima, laborum illo enim hic ratione maxime possimus.
+	Asperiores rem beatae illo?
+</p>
+<p class="filler">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem quasi dolore inventore eum
+	voluptas provident molestiae! Inventore minima, laborum illo enim hic ratione maxime possimus.
+	Asperiores rem beatae illo?
+</p>
+<p><a href="/">Return to Home.</a></p>
+
+<style>
+	.filler {
+		font-size: 32px;
+	}
+</style>
---
+++ src/routes/dragon-builder/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Dragon Builder</h1>
+<p>WIP. Once complete, you'll be able to build dragons here.</p>
+<p><a href="/">Return to Home.</a></p>
