8bb4c2807fa5c816c590d479222c41428ac82430
Peter Duerst <peterad12@gmail.com>
2023-08-07 11:57:32 -0400

    Converted to AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,23 +6,82 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 
 	import '../app.postcss';
-	import Header from '$lib/header/Header.svelte';
-	import Footer from '$lib/footer/Footer.svelte';
+
+	import { AppShell, AppBar, Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/Navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
-<Header />
+<Drawer>
+	<h2 class="p-4">Navigation</h2>
+	<hr />
+	<Navigation />
+</Drawer>
 
-<main class="bg-surface-500">
-	<div class="main-contents-wrapper">
-		<slot />
-	</div>
-</main>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">Skeleton</strong>
+				</div>
+			</svelte:fragment>
 
-<Footer />
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<main>
+		<div class="main-contents-wrapper">
+			<slot />
+		</div>
+	</main>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
 
 <style lang="scss">
 	main {
-		width: 100%;
+		text-align: center;
 	}
 
 	.main-contents-wrapper {
