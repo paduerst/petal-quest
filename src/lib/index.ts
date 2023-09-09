3e07f5a2507931a2f9cc42e1d6a04b644c2a2c32
Peter Duerst <peterad12@gmail.com>
2023-09-08 22:48:42 -0400

    Changing to use trailingSlash always
    This is an attempt at fixing a bug caused by my domain provider's choice to always add a trailing slash when forwarding a URL.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,9 +2,9 @@ export const NAV_LINKS: readonly {
 	href: string;
 	text: string;
 }[] = [
-	{ href: '/prismatic-dragons', text: 'Prismatic Dragons' },
-	{ href: '/dragon-builder', text: 'Dragon Builder' },
-	{ href: '/about', text: 'About' }
+	{ href: '/prismatic-dragons/', text: 'Prismatic Dragons' },
+	{ href: '/dragon-builder/', text: 'Dragon Builder' },
+	{ href: '/about/', text: 'About' }
 ] as const;
 
 export const BASE_SHARE_URL = 'https://dragon.petal.quest/';
