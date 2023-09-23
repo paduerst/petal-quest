9e133aef5528ff0cbc25b2bc447bb3d315235b9d
Peter Duerst <peterad12@gmail.com>
2023-09-23 16:22:25 -0400

    WIP dropdown menu for navigation

--- src/lib/HeaderNavLink.svelte
+++ src/lib/HeaderNavLink.svelte
@@ -11,34 +11,32 @@
 </script>
 
 <li
-	class="m-1 h-fit w-fit flex flex-nowrap hover:bg-primary-hover-token rounded-[var(--theme-rounded-base)]"
-	class:active={path === link.href}
+	class="m-1 h-fit w-fit rounded-[var(--theme-rounded-base)] daisy-dropdown daisy-dropdown-hover"
+	class:bg-surface-active-token={path === link.href}
 >
-	<a
-		class="nav-anchor p-2 rounded-[var(--theme-rounded-base)] hover:bg-primary-hover-token"
-		class:rounded-r-none={link.children.length > 0}
-		class:pr-1={link.children.length > 0}
-		href={link.href}
-	>
-		{link.text}
-	</a>
-	{#if link.children.length > 0}
-		<button
-			class="w-fit pl-1 pr-1 rounded-r-[var(--theme-rounded-base)] hover:bg-primary-hover-token text-center flex items-center justify-center"
+	<label for="blahg">
+		<a
+			class="p-1 rounded-[var(--theme-rounded-base)] text-lg leading-8 no-underline text-token box-border hover:bg-primary-hover-token flex flex-nowrap"
+			href={link.href}
 		>
-			<ChevronDown class="mt-1" width="1.25rem" height="1.25rem" />
-		</button>
+			<span class="p-1">{link.text}</span>
+
+			{#if link.children.length > 0}
+				<div class="w-fit p-1 text-center flex items-center justify-center">
+					<ChevronDown class="mt-1" width="1.25rem" height="1.25rem" />
+				</div>
+			{/if}
+		</a>
+	</label>
+
+	{#if link.children.length > 0}
+		<ul
+			id="blahg"
+			class="daisy-dropdown-content z-[1] daisy-menu p-2 shadow bg-surface-400-500-token w-52 border-surface-300-600-token"
+		>
+			{#each link.children as child}
+				<li><a href={child.href}>{child.text}</a></li>
+			{/each}
+		</ul>
 	{/if}
 </li>
-
-<style>
-	.nav-anchor {
-		@apply no-underline text-token box-border;
-		font-size: 1.125rem /* 18px */;
-		line-height: 2rem /* 32px */;
-	}
-
-	.active {
-		@apply bg-surface-active-token;
-	}
-</style>
