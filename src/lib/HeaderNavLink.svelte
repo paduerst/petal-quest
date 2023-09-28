a0201c84e1bba75929077c410923af7206a8e3cb
Peter Duerst <peterad12@gmail.com>
2023-09-28 19:52:05 -0400

    WIP NavLink dropdown formatting

--- src/lib/HeaderNavLink.svelte
+++ src/lib/HeaderNavLink.svelte
@@ -8,15 +8,16 @@
 	$: path = $page.url.pathname;
 
 	export let link: (typeof NAV_LINKS)[number];
+	let linkWidth: number;
 </script>
 
 <li
-	class="m-1 h-fit w-fit rounded-[var(--theme-rounded-base)] daisy-dropdown daisy-dropdown-hover"
+	class="m-1 h-fit w-fit rounded-[--theme-rounded-base] daisy-dropdown daisy-dropdown-hover"
 	class:bg-surface-active-token={path === link.href}
 >
-	<label for="blahg">
+	<label for="blahg" bind:clientWidth={linkWidth}>
 		<a
-			class="p-1 rounded-[var(--theme-rounded-base)] text-lg leading-8 no-underline text-token box-border hover:bg-primary-hover-token flex flex-nowrap"
+			class="p-1 rounded-[--theme-rounded-base] text-lg leading-8 no-underline text-token box-border hover:bg-primary-hover-token flex flex-nowrap"
 			href={link.href}
 		>
 			<span class="p-1">{link.text}</span>
@@ -32,10 +33,21 @@
 	{#if link.children.length > 0}
 		<ul
 			id="blahg"
-			class="daisy-dropdown-content z-[1] daisy-menu p-2 shadow bg-surface-400-500-token w-52 border-surface-300-600-token"
+			class="daisy-dropdown-content z-[1] p-2 shadow bg-surface-200-700-token w-fit border-surface-300-600-token border rounded-[--theme-rounded-base]"
+			style="min-width: {linkWidth}px;"
 		>
 			{#each link.children as child}
-				<li><a href={child.href}>{child.text}</a></li>
+				<li
+					class="m-1 h-fit w-full rounded-[--theme-rounded-base]"
+					class:bg-surface-active-token={path === child.href}
+				>
+					<a
+						class="p-1 w-full rounded-[--theme-rounded-base] text-lg leading-8 no-underline text-token box-border hover:bg-primary-hover-token flex flex-nowrap"
+						href={child.href}
+					>
+						{child.text}
+					</a>
+				</li>
 			{/each}
 		</ul>
 	{/if}
