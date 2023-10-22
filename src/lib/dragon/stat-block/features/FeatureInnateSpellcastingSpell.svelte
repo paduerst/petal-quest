0accf0b6abdda90feb9414a5ad5ed2216c1d7265
Peter Duerst <peterad12@gmail.com>
2023-10-22 12:40:47 -0400

    Added SpellLink + support for nested parentheses

--- src/lib/dragon/stat-block/features/FeatureInnateSpellcastingSpell.svelte
+++ src/lib/dragon/stat-block/features/FeatureInnateSpellcastingSpell.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
-	export let spell: string;
-	spell.trim();
+	import SpellLink from '$lib/spells/SpellLink.svelte';
 
-	const spellArr = spell.split('(');
+	export let spell: string;
+	$: spell.trim();
+
+	let spellArr: string[];
+	$: spellArr = spell.split('(');
+
+	let spellName: string;
+	$: spellName = spellArr[0].trim();
+
+	let spellParenthetical: string;
+	$: if (spellArr.length > 1) {
+		spellParenthetical = ` (${spellArr.slice(1).join('(')}`;
+	} else {
+		spellParenthetical = '';
+	}
 </script>
 
-<i>{spellArr[0].trim()}</i>{#if spellArr.length > 1}{` (${spellArr[1].trim()}`}{/if}
+<SpellLink {spellName} />{spellParenthetical}
---
+++ src/lib/spells/SpellLink.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let spellName: string;
+</script>
+
+<i>
+	{spellName}
+</i>
