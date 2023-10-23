9b791d14f36425bf64456ae7ea17d588ab29cf1c
Peter Duerst <peterad12@gmail.com>
2023-10-23 16:23:07 -0400

    Added rider effect to Variable Radiance

--- src/lib/dragon/stat-block/bonus-actions/BActionVariableRadiance.svelte
+++ src/lib/dragon/stat-block/bonus-actions/BActionVariableRadiance.svelte
@@ -2,6 +2,8 @@
 	import type { DragonStats } from '$lib/dragon/dragon-stats';
 
 	export let dragon: DragonStats;
+
+	const ADDED_RIDER_EFFECT = true;
 </script>
 
 <div class="dragon-action">
@@ -9,8 +11,12 @@
 		<i><b>Variable Radiance.</b></i>
 		{dragon.nameUpper} chooses a radius up to {dragon.prismaticRadianceRadius}
 		feet and glows {dragon.color}, shedding bright light in the chosen radius and dim light for an
-		additional distance equal to the chosen radius. {dragon.nameUpper} stops glowing if
-		{dragon.pronounNominative}
+		additional distance equal to the chosen radius.
+		{#if ADDED_RIDER_EFFECT}
+			Creatures within this bright light have a −1 penalty to their saving throws against {dragon.name}'s
+			{dragon.breath2Name} Breath.
+		{/if}
+		{dragon.nameUpper} stops glowing if {dragon.pronounNominative}
 		{dragon.pronounsPlural ? 'die' : 'dies'} or
 		{dragon.pronounsPlural ? 'choose' : 'chooses'} to stop as a bonus action.
 	</p>
