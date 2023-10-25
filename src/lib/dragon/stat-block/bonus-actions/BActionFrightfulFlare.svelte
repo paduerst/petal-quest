7d0c4a13f46d0c52682fea2d461338c35541e6dd
Peter Duerst <peterad12@gmail.com>
2023-10-25 13:06:16 -0400

    Variable Radiance updated, penalizes Flare saves

--- src/lib/dragon/stat-block/bonus-actions/BActionFrightfulFlare.svelte
+++ src/lib/dragon/stat-block/bonus-actions/BActionFrightfulFlare.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { DragonStats } from '$lib/dragon/dragon-stats';
+	import { FLARE_DISADVANTAGE_FOR_CLOSE_CREATURES } from '.';
 
 	export let dragon: DragonStats;
 </script>
@@ -13,12 +14,13 @@
 			{dragon.nameUpper} unleashes {dragon.pronounPossessiveAdjective} inner light, maxing out {dragon.pronounPossessiveAdjective}
 			Variable Radiance to a radius of {dragon.prismaticRadianceRadius} feet. Each creature of {dragon.name}'s
 			choice within {2 * dragon.prismaticRadianceRadius} feet of {dragon.pronounObjective} must make
-			a
-			<span class="whitespace-nowrap">DC {dragon.saveDCCha}</span>
-			Wisdom saving throw if it can see {dragon.name}. Creatures within {dragon.prismaticRadianceRadius}
-			feet of {dragon.pronounObjective}
-			have disadvantage on this save. On a failed save, a creature is frightened until the end of {dragon.name}'s
-			next turn.
+			a <span class="whitespace-nowrap">DC {dragon.saveDCCha}</span> Wisdom saving throw if it can
+			see {dragon.name}.
+			{#if FLARE_DISADVANTAGE_FOR_CLOSE_CREATURES}
+				Creatures within {dragon.prismaticRadianceRadius} feet of
+				{dragon.pronounObjective} have disadvantage on this save.
+			{/if}
+			On a failed save, a creature is frightened until the end of {dragon.name}'s next turn.
 		</p>
 	</div>
 {/if}
--- src/lib/dragon/stat-block/bonus-actions/BActionVariableRadiance.svelte
+++ src/lib/dragon/stat-block/bonus-actions/BActionVariableRadiance.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import type { DragonStats } from '$lib/dragon/dragon-stats';
+	import {
+		RADIANCE_PENALTY,
+		RADIANCE_PENALIZES_SECONDARY_BREATH_SAVES,
+		RADIANCE_PENALIZES_FRIGHTFUL_FLARE_SAVES
+	} from '.';
 
 	export let dragon: DragonStats;
 
-	const ADDED_RIDER_EFFECT = true;
+	let penalties: string;
+	$: {
+		penalties = '';
+		if (RADIANCE_PENALIZES_SECONDARY_BREATH_SAVES) {
+			penalties += `${dragon.breath2Name} Breath`;
+		}
+		if (RADIANCE_PENALIZES_SECONDARY_BREATH_SAVES && RADIANCE_PENALIZES_FRIGHTFUL_FLARE_SAVES) {
+			penalties += ' and ';
+		}
+		if (RADIANCE_PENALIZES_FRIGHTFUL_FLARE_SAVES) {
+			penalties += 'Frightful Flare';
+		}
+	}
 </script>
 
 <div class="dragon-action">
@@ -12,9 +29,10 @@
 		{dragon.nameUpper} chooses a radius up to {dragon.prismaticRadianceRadius}
 		feet and glows {dragon.color}, shedding bright light in the chosen radius and dim light for an
 		additional distance equal to the chosen radius.
-		{#if ADDED_RIDER_EFFECT}
-			Creatures within this bright light have a −1 penalty to their saving throws against {dragon.name}'s
-			{dragon.breath2Name} Breath.
+		{#if RADIANCE_PENALIZES_SECONDARY_BREATH_SAVES || RADIANCE_PENALIZES_FRIGHTFUL_FLARE_SAVES}
+			Creatures within this bright light have a −{RADIANCE_PENALTY} penalty to their saving throws against
+			{dragon.name}'s
+			{penalties}.
 		{/if}
 		{dragon.nameUpper} stops glowing if {dragon.pronounNominative}
 		{dragon.pronounsPlural ? 'die' : 'dies'} or
