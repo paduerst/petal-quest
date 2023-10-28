7296ed25597dd92c7c7d97818ac143dea327707f
Peter Duerst <peterad12@gmail.com>
2023-10-27 23:15:42 -0400

    Corrected AbbrButton label placeholder

--- src/lib/dragon/stat-block/AbbrButton.svelte
+++ src/lib/dragon/stat-block/AbbrButton.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { normalizeString } from '$lib/text-utils';
+
 	export let title: string;
+
+	let abbrID = `abbrevationDropdown__${normalizeString(title)}`;
 </script>
 
 <div class="daisy-dropdown daisy-dropdown-hover">
-	<label for="testfsdf">
+	<label for={abbrID}>
 		<button class="underline decoration-dotted"><slot /></button>
 	</label>
 	<div
+		id={abbrID}
 		class="daisy-card daisy-compact daisy-dropdown-content z-[1] shadow bg-base-100 rounded-box border border-black min-w-[8rem]"
 	>
 		<div class="daisy-card-body">
