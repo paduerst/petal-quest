0435b8d3ca3cae2acc5baf29701d2d9151f00f17
Peter Duerst <peterad12@gmail.com>
2023-11-26 19:37:00 -0500

    WIP Modal for Abbr

---
+++ src/lib/modals/AbbrModal.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	// Props
+	/** Exposes parent props to this component. */
+	export let parent: {
+		position: string;
+		// ---
+		background: string;
+		width: string;
+		height: string;
+		padding: string;
+		spacing: string;
+		rounded: string;
+		shadow: string;
+		// ---
+		buttonNeutral: string;
+		buttonPositive: string;
+		buttonTextCancel: string;
+		buttonTextConfirm: string;
+		buttonTextSubmit: string;
+		// ---
+		regionBackdrop: string;
+		regionHeader: string;
+		regionBody: string;
+		regionFooter: string;
+		// ---
+		onClose: () => void;
+	};
+
+	// Stores
+	import { getModalStore } from '@skeletonlabs/skeleton';
+	const modalStore = getModalStore();
+
+	let abbrInfo: { abbr: string; title: string } = $modalStore[0].value;
+</script>
+
+{#if $modalStore[0]}
+	<div class="card p-4 w-modal shadow-xl space-y-4 overflow-x-clip">
+		<header class="text-2xl font-bold">{abbrInfo.abbr}</header>
+		<div>{abbrInfo.title}</div>
+		<footer class="modal-footer {parent.regionFooter}">
+			<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}> Close </button>
+		</footer>
+	</div>
+{/if}
