d07dfd557690e1b4fa3e265686707ff4280091e8
Peter Duerst <peterad12@gmail.com>
2023-12-07 17:19:08 -0500

    Updated scholar art alt text

--- src/routes/scholar/+page.svelte
+++ src/routes/scholar/+page.svelte
@@ -17,7 +17,7 @@
 		<figure class="shrink-0 w-fit mx-auto">
 			<img
 				src="/images/scholar_glasses_with_scroll.jpg"
-				alt="An illustration of glasses on an unfurled scroll covered in arcane runes."
+				alt="An illustration of glasses on an unfurled scroll covered in ancient text."
 				class="h-64 aspect-square shrink-0"
 			/>
 			<figcaption class="italic m-1">Artwork by Tal S.</figcaption>
