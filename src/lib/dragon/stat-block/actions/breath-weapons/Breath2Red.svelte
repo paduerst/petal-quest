63c8ce89701542aa70d4278d68c2877ebbfa7637
Peter Duerst <peterad12@gmail.com>
2023-12-13 12:00:28 -0500

    Clarified Fury Breath movement

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath2Red.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath2Red.svelte
@@ -19,9 +19,9 @@
 		for 1 minute. On each of its turns while <ConditionLink
 			condition="charmed"
 			disabled={disableLinks}
-		/> in this way, the creature must move towards the nearest creature it can see (excluding itself
-		and {dragon.name}) and attack it once, ending its turn after doing so or if unable to. The
-		creature can repeat the saving throw at the end of each of its turns, ending the effect on
+		/> in this way, the creature must move as close as possible to the nearest creature it can see (excluding
+		itself and {dragon.name}) and attack it once, ending its turn after doing so or if unable to.
+		The creature can repeat the saving throw at the end of each of its turns, ending the effect on
 		itself with a success.
 	</p>
 </div>
