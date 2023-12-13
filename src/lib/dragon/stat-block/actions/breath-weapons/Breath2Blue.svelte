445f95759197208c3d87f269d8037bc3de518734
Peter Duerst <peterad12@gmail.com>
2023-12-12 23:06:28 -0500

    Ward of Light as a Feature, True Polymorph Breath, and a small Psychic Breath edit (#47)
    * Rewrote Ward of Light as a feature
    
    Also added nowrap to a span in the yellow breath weapon.
    
    * Blue Secondary Breath now becomes True Polymorph
    
    * Clarified Psychic Breath save success

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath2Blue.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath2Blue.svelte
@@ -2,7 +2,6 @@
 	import { capitalizeFirstLetter } from '$lib/text-utils';
 	import type { DragonStats } from '$lib/dragon/dragon-stats';
 	import SpellLink from '$lib/spells/SpellLink.svelte';
-	import ConditionLink from '$lib/conditions/ConditionLink.svelte';
 	import MonsterLink from '$lib/monsters/MonsterLink.svelte';
 
 	export let dragon: DragonStats;
@@ -18,19 +17,29 @@
 <div class="dragon-action breath-option">
 	{#if POLYMORPH_BREATH}
 		<p>
-			<b>Polymorph Breath.</b>
+			<b>{dragon.breath2Name} Breath.</b>
 			{capitalizeFirstLetter(dragon.color)} rays of magical light flash from {dragon.name}'s mouth
 			in a {dragon.breathConeSize}-foot cone. Each creature in that area must make a DC {dragon.saveDCCon}
 			Wisdom saving throw. On a failed save, the creature is transformed into a <MonsterLink
 				monster={'frog'}
 				disabled={disableLinks}
 			/>, as if by the
-			<SpellLink spellName="polymorph" disabled={disableLinks} /> spell with a duration of 1 minute (no
-			concentration).
+			<SpellLink spellName="polymorph" disabled={disableLinks} /> spell
+			{#if dragon.breath2SpecialValue.length > 0}
+				with a duration of
+				<span class="whitespace-nowrap">{dragon.breath2SpecialValue}</span>
+			{/if}
+			(no concentration).
+			{#if dragon.breath2Name === 'True Polymorph'}
+				If the creature is still a <MonsterLink monster={'frog'} disabled={disableLinks} /> at the end
+				of its next turn, it must make a DC {dragon.saveDCCon}
+				Charisma saving throw. If it fails this second save as well, the creature's transformation becomes
+				permanent, lasting until dispelled (DC 19).
+			{/if}
 		</p>
 	{:else}
 		<p>
-			<b>{dragon.breath2Name} Breath.</b>
+			<b>Wild Magic Breath.</b>
 			{capitalizeFirstLetter(dragon.color)} rays of magical light flash from {dragon.name}'s mouth
 			in a {dragon.breathConeSize}-foot cone. Randomly select one of the following spells; each
 			creature in that area is targeted by that spell (spell save DC {dragon.saveDCCon}). The
