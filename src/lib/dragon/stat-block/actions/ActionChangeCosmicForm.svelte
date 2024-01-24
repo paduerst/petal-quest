40ab68a8e2b0f4b1b8445d123a884f8bcb7e0063
Peter Duerst <peterad12@gmail.com>
2024-01-24 14:41:12 -0500

    Change Cosmic Form ready for playtesting

--- src/lib/dragon/stat-block/actions/ActionChangeCosmicForm.svelte
+++ src/lib/dragon/stat-block/actions/ActionChangeCosmicForm.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { capitalizeFirstLetter } from '$lib/text-utils';
 	import type { DragonStats } from '$lib/dragon/dragon-stats';
 
 	export let dragon: DragonStats;
@@ -8,8 +9,15 @@
 	<div class="dragon-action">
 		<p>
 			<i><b>Change Cosmic Form.</b></i>
-			{dragon.nameUpper} enters or attempts to exit
-			{dragon.pronounPossessiveAdjective} cosmic form.
+			{dragon.nameUpper} enters
+			{dragon.pronounPossessiveAdjective} cosmic form, or {dragon.pronounNominative}
+			{dragon.pronounsPlural && dragon.pronounNominativeExists ? 'attempt' : 'attempts'} to exit it.
+			To exit {dragon.pronounPossessiveAdjective} cosmic form, {dragon.name} must succeed on a DC 30
+			Constitution saving throw. {capitalizeFirstLetter(dragon.pronounNominative)}
+			{dragon.pronounsPlural && dragon.pronounNominativeExists ? 'make' : 'makes'} this save with disadvantage
+			if {dragon.pronounNominative}
+			{dragon.pronounsPlural && dragon.pronounNominativeExists ? 'have' : 'has'} half of {dragon.pronounPossessiveAdjective}
+			hit points remaining or fewer.
 		</p>
 	</div>
 {/if}
