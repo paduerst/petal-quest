8e755d825dd9376675ce513012435396fcd1954e
Peter Duerst <peterad12@gmail.com>
2024-01-25 12:47:55 -0500

    Tweaked Void Breath wording

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath2Black.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath2Black.svelte
@@ -16,9 +16,9 @@
 		extinguished within that area, and each creature in that area must make a DC {dragon.saveDCCon}
 		Charisma saving throw. On a failed save, the creature is cursed {dragon.breath2SpecialValue}until
 		freed by the
-		<SpellLink spellName="remove curse" disabled={disableLinks} /> spell or similar magic. The cursed
-		creature is <ConditionLink condition="blinded" disabled={disableLinks} />, has its speed halved,
-		and does not age. The curse manifests as an intangible shroud of translucent shadow draped over
-		the creature.
+		<SpellLink spellName="remove curse" disabled={disableLinks} /> spell or similar magic. While cursed
+		in this way, the creature is <ConditionLink condition="blinded" disabled={disableLinks} />, has
+		its speed halved, and does not age. The curse manifests as a translucent, intangible shroud of
+		shadow that drapes over the creature.
 	</p>
 </div>
