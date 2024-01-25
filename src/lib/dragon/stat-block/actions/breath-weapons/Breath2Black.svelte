bc94ecaa03b462353e144ad0469e035f9c1c2e10
Peter Duerst <peterad12@gmail.com>
2024-01-24 19:24:28 -0500

    Improved Indigo and Black Secondary Breath wording

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath2Black.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath2Black.svelte
@@ -16,8 +16,9 @@
 		extinguished within that area, and each creature in that area must make a DC {dragon.saveDCCon}
 		Charisma saving throw. On a failed save, the creature is cursed {dragon.breath2SpecialValue}until
 		freed by the
-		<SpellLink spellName="remove curse" disabled={disableLinks} /> spell or other magic. While cursed
-		in this way, the creature has its speed halved, it does not age, and it is cloaked in a shroud of
-		shadow which <ConditionLink condition="blinded" displayText="blinds" disabled={disableLinks} /> it.
+		<SpellLink spellName="remove curse" disabled={disableLinks} /> spell or similar magic. The cursed
+		creature is <ConditionLink condition="blinded" disabled={disableLinks} />, has its speed halved,
+		and does not age. The curse manifests as an intangible shroud of translucent shadow draped over
+		the creature.
 	</p>
 </div>
