d20020a53ce245333fe30efc82cf0291abb0cad9
Peter Duerst <peterad12@gmail.com>
2024-01-26 18:53:34 -0500

    Simplified Cold Breath

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath1Blue.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath1Blue.svelte
@@ -8,9 +8,8 @@
 	<p>
 		<b>{dragon.breath1Name} Breath.</b>
 		{dragon.nameUpper} exhales an icy blast in a {dragon.breathConeSize}-foot cone. Each creature in
-		that area must make a DC {dragon.saveDCCon} Constitution saving throw. On a failed save, the creature
-		takes {dragon.breath1ExpectedDamage} ({dragon.breath1DiceCount}d{dragon.breath1DiceType}) cold
-		damage and has its speed halved until the end of its next turn. On a successful save, the
-		creature takes half as much damage and doesn't have its speed halved.
+		that area must make a DC {dragon.saveDCCon} Constitution saving throw, taking {dragon.breath1ExpectedDamage}
+		({dragon.breath1DiceCount}d{dragon.breath1DiceType}) cold damage on a failed save, or half as
+		much damage on a successful one.
 	</p>
 </div>
