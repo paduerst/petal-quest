7f4869f65379af03389b0cba8cbb9e6ec2aab1e8
Peter Duerst <peterad12@gmail.com>
2024-02-05 14:18:16 -0500

    Restructured Cosmic Form for clarity, no spells

--- src/lib/dragon/stat-block/features/FeatureCosmicForm.svelte
+++ src/lib/dragon/stat-block/features/FeatureCosmicForm.svelte
@@ -16,7 +16,8 @@
 	const novaBlindingDistance = novaMovementDistance;
 
 	let variableTraitName = dragon.cosmicForm === 'Supernova' ? 'Radiance' : 'Shadow';
-
+	let wardTraitName = dragon.cosmicForm === 'Supernova' ? 'Light' : 'Shadow';
+	let novaTraitName = dragon.cosmicForm === 'Supernova' ? 'Exploding' : 'Imploding';
 	let novaVerb = dragon.cosmicForm === 'Supernova' ? 'explodes' : 'implodes';
 </script>
 
@@ -27,46 +28,64 @@
 			If {dragon.name}'s current hit points drop below half of {dragon.pronounPossessiveAdjective}
 			hit point maximum, {dragon.name}
 			enters {dragon.pronounPossessiveAdjective} cosmic form: a {dragon.cosmicForm.toLowerCase()}.
-			While in {dragon.pronounPossessiveAdjective} cosmic form, {dragon.name} can hover, {dragon.pronounPossessiveAdjective}
-			speed is halved, and {dragon.pronounPossessiveAdjective} Variable {variableTraitName} is maxed
-			out at a radius of {dragon.prismaticRadianceRadius} feet.
-			{#if dragon.cosmicForm === 'Supernova'}
-				If another creature that can see {dragon.name} moves to within {novaBlindingDistance} feet of
-				{dragon.pronounObjective} or starts its turn there, the creature must succeed on a
-				<span class="whitespace-nowrap">DC {dragon.saveDCCon}</span> Constitution saving throw or be
-				<ConditionLink condition="blinded" disabled={disableLinks} /> until the start of its next turn.
-			{:else}
-				The area within {novaBlindingDistance} feet of {dragon.name} is utterly devoid of light; it is
-				filled with darkness that can't be illuminated by any means or seen through with darkvision.
-			{/if}
+			While in {dragon.pronounPossessiveAdjective} cosmic form, {dragon.name} is subject to the following
+			effects.
 		</p>
-		<p>
-			At the start of each of {dragon.name}'s turns while in {dragon.pronounPossessiveAdjective} cosmic
-			form, {dragon.name}
-			{novaVerb}; each creature within {dragon.prismaticRadianceRadius}
-			feet of {dragon.name} must make a <span class="whitespace-nowrap">DC {dragon.saveDCCon}</span>
-			Dexterity or Constitution saving throw (the creature's choice). On a failed save, the creature
-			takes
-			<span class="whitespace-nowrap"
-				>{expectedDiceResult(novaDiceCount, novaDiceType)} ({novaDiceCount}d{novaDiceType})</span
-			>
-			{dragon.immunity.toLowerCase()}
-			damage and
-			{#if dragon.cosmicForm === 'Supernova'}
-				is pushed {novaMovementDistance} feet away from {dragon.name}. On a successful save, the
-				creature takes half as much damage and isn't pushed.
-			{:else}
-				is pulled {novaMovementDistance} feet closer to {dragon.name}. On a successful save, the
-				creature takes half as much damage and isn't pulled.
-			{/if}
-
-			Simultaneously, {dragon.name}
-			loses hit points equal to the damage roll.
-		</p>
-		<p>
-			If {dragon.name} drops to 0 hit points while in {dragon.pronounPossessiveAdjective} cosmic form,
-			{dragon.name} immediately dies and {dragon.pronounPossessiveAdjective} body is destroyed without
-			a trace.
-		</p>
+		<ul class="stat-block-unordered-list">
+			<li>
+				<b>Hovering.</b>
+				{dragon.nameUpper} can hover and {dragon.pronounPossessiveAdjective}
+				speed is halved.
+			</li>
+			<li>
+				<b>No Spells.</b>
+				{dragon.nameUpper} cannot cast spells or concentrate on them.
+			</li>
+			<li>
+				<b>Maximum {variableTraitName}.</b>
+				{dragon.nameUpper}'s Variable {variableTraitName} is maxed out at a radius of {dragon.prismaticRadianceRadius}
+				feet.
+				{#if dragon.cosmicForm === 'Supernova'}
+					If another creature that can see {dragon.name} moves to within {novaBlindingDistance} feet
+					of
+					{dragon.pronounObjective} or starts its turn there, the creature must succeed on a
+					<span class="whitespace-nowrap">DC {dragon.saveDCCon}</span> Constitution saving throw or
+					be
+					<ConditionLink condition="blinded" disabled={disableLinks} /> until the start of its next turn.
+				{:else}
+					The area within {novaBlindingDistance} feet of {dragon.name} is utterly devoid of light; it
+					is filled with darkness that can't be illuminated by any means or seen through with darkvision.
+				{/if}
+			</li>
+			<li>
+				<b>{novaTraitName}.</b>
+				At the start of each of {dragon.name}'s turns, {dragon.name}
+				{novaVerb}; each creature within {dragon.prismaticRadianceRadius}
+				feet of {dragon.name} must make a
+				<span class="whitespace-nowrap">DC {dragon.saveDCCon}</span>
+				Dexterity or Constitution saving throw (the creature's choice). On a failed save, the creature
+				takes
+				<span class="whitespace-nowrap"
+					>{expectedDiceResult(novaDiceCount, novaDiceType)} ({novaDiceCount}d{novaDiceType})</span
+				>
+				{dragon.immunity.toLowerCase()}
+				damage and
+				{#if dragon.cosmicForm === 'Supernova'}
+					is pushed {novaMovementDistance} feet away from {dragon.name}. On a successful save, the
+					creature takes half as much damage and isn't pushed.
+				{:else}
+					is pulled {novaMovementDistance} feet closer to {dragon.name}. On a successful save, the
+					creature takes half as much damage and isn't pulled.
+				{/if}
+				Simultaneously, {dragon.name}
+				loses hit points equal to the damage roll.
+			</li>
+			<li>
+				<b>Destruction.</b>
+				If {dragon.name} drops to 0 hit points,
+				{dragon.name} immediately dies and {dragon.pronounPossessiveAdjective} body is destroyed without
+				a trace, bypassing {dragon.pronounPossessiveAdjective} Ward of {wardTraitName}.
+			</li>
+		</ul>
 	</div>
 {/if}
