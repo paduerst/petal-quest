93976f4516613bb9cf6c9b2ebf3cba3ae15fabf5
Peter Duerst <peterad12@gmail.com>
2024-07-07 11:16:54 -0400

    Rainbow Breath now uses &nbsp; to prevent awkward line breaks

--- src/lib/dragon/stat-block/actions/breath-weapons/Breath1White.svelte
+++ src/lib/dragon/stat-block/actions/breath-weapons/Breath1White.svelte
@@ -11,7 +11,8 @@
 		creature in that area must make a DC {dragon.saveDCCon} Dexterity saving throw and roll a d8. On
 		a failed save, the creature takes {dragon.breath1ExpectedDamage}
 		({dragon.breath1DiceCount}d{dragon.breath1DiceType}) damage of a type determined by the d8 roll:
-		fire (1), acid (2), lightning (3), poison (4), cold (5), thunder (6), force (7), radiant (8). On
-		a successful save, the creature takes half as much damage.
+		fire&nbsp;(1), acid&nbsp;(2), lightning&nbsp;(3), poison&nbsp;(4), cold&nbsp;(5),
+		thunder&nbsp;(6), force&nbsp;(7), radiant&nbsp;(8). On a successful save, the creature takes
+		half as much damage.
 	</p>
 </div>
